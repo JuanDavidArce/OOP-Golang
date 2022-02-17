11c004dd7a572d8d117e887e3690a5164acc8f29
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-17 16:47:57 -0500

    Interface

--- poo/class.go
+++ poo/class.go
@@ -14,16 +14,37 @@ type Employee struct {
 type FullTimeEmployee struct {
 	Person
 	Employee
+	endDate string
 }
 
-func GetMessage(p Person) {
-	fmt.Println("%s with age %d", p.name, p.age)
+type TemporaryEmployee struct {
+	Person
+	Employee
+	taxRate int
 }
+
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full time employee"
+}
+func (tEmployee TemporaryEmployee) getMessage() string {
+	return "Temporary time employee"
+}
+
+type PrintInfo interface {
+	getMessage() string
+}
+
+func getMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
+}
+
 func main() {
-	ftEmplyee := FullTimeEmployee{}
-	ftEmplyee.name = "Juan David"
-	ftEmplyee.age = 2
-	ftEmplyee.id = 15
-	fmt.Println(ftEmplyee)
-	GetMessage(ftEmplyee)
+	ftEmployee := FullTimeEmployee{}
+	ftEmployee.name = "Juan David"
+	ftEmployee.age = 2
+	ftEmployee.id = 15
+	fmt.Println(ftEmployee)
+	tEmployee := TemporaryEmployee{}
+	getMessage(tEmployee)
+	getMessage(ftEmployee)
 }
