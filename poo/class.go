98efc5bb3b826dd66a082bd9ca355ea2ba0677f9
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-17 16:15:56 -0500

    constructor

--- poo/class.go
+++ poo/class.go
@@ -3,31 +3,39 @@ package main
 import "fmt"
 
 type Employee struct {
-	id   int
-	name string
+	id       int
+	name     string
+	vacation bool
 }
 
-func (e *Employee) SetId(id int) {
-	e.id = id
-}
-func (e *Employee) SetName(name string) {
-	e.name = name
-}
-func (e *Employee) GetId() int {
-	return e.id
-}
-func (e *Employee) GetName() string {
-	return e.name
+func NewEmployee(id int, name string, vacation bool) *Employee {
+	return &Employee{
+		id:       id,
+		name:     name,
+		vacation: vacation,
+	}
 }
 
 func main() {
+	// 1
 	e := Employee{}
 	fmt.Println(e)
-	e.id = 1
-	e.name = "Name"
-	fmt.Println(e)
-	e.SetId(5)
-	fmt.Println(e)
-	fmt.Println(e.GetId(), e.GetName())
 
+	// 2
+	e2 := Employee{
+		id:       1,
+		name:     "Name",
+		vacation: true,
+	}
+	fmt.Println(e2)
+
+	// 3
+	e3 := new(Employee)
+	fmt.Println(*e3)
+	e3.id = 1
+	e3.name = "Name"
+	fmt.Println(*e3)
+
+	e4 := NewEmployee(1, "Names", false)
+	fmt.Println(*e4)
 }
