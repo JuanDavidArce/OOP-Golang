0642d44b7211929c26baf8a675d879c3367d0bc3
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-16 22:35:58 -0500

    methods

--- poo/class.go
+++ poo/class.go
@@ -7,11 +7,27 @@ type Employee struct {
 	name string
 }
 
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+func (e *Employee) GetId() int {
+	return e.id
+}
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
 	fmt.Println(e)
 	e.id = 1
 	e.name = "Name"
 	fmt.Println(e)
+	e.SetId(5)
+	fmt.Println(e)
+	fmt.Println(e.GetId(), e.GetName())
 
 }
